e54e5aeca150aced6e74e3fac1b6bb4d20dac148
Kelsey Hrubes <kelseyhrubes@gmail.com>
2015-12-28 16:46:59 -0600

    add slow scroll for main nav bar

--- js/init.js
+++ js/init.js
@@ -33,4 +33,20 @@ var INITIAL_NAV_OFFSET = $("nav").offset().top;
    	}
 }
 
+/* Smooth scrolling function */
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      console.log(target);
+
+      if (target.length) {
+        $('html,body').animate({ scrollTop:  (target.selector == "#about-text") ? (INITIAL_NAV_OFFSET + 1) : (target.offset().top - 50)  }, 1500);
+        return false;
+      }
+    }
+  });
+});
+
 })(jQuery); // end of jQuery name space
